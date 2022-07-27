7f85fabad0c12cf695a7baa1de7acc1001173861
steve2chao <steve2chao@gmail.com>
2022-07-27 11:25:22 -0400

    Added SecurityUtil.java

---
+++ src/SecurityUtil.java
@@ -0,0 +1,26 @@
+import org.owasp.html.Handler;
+import org.owasp.html.HtmlPolicyBuilder;
+import org.owasp.html.HtmlStreamRenderer;
+import org.owasp.html.HtmlSanitizer;
+
+public class SecurityUtil {
+	
+	public static String sanitizeAllowCommonFormat(String content) {
+		HtmlPolicyBuilder policyBuilder = new HtmlPolicyBuilder().allowCommonInlineFormattingElements();
+		return sanitize(content, policyBuilder);
+	}
+	
+	public static String sanitize(String content, HtmlPolicyBuilder policyBuilder) {
+		StringBuilder sb = new StringBuilder();
+		HtmlStreamRenderer renderer = HtmlStreamRenderer.create(sb, new Handler<String>() {
+			public void handle(String errorMessage) {
+				System.out.println("Error occurred while attempting to render content: "+errorMessage);
+			}
+		});
+		
+		HtmlSanitizer.Policy policy = policyBuilder.toFactory().apply(renderer);
+		HtmlSanitizer.sanitize(content, policy);
+		
+		return sb.toString();
+	}
+}
